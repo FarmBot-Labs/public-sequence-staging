969c558f6f9ba3d4bcf80498f0994ef7b671f9c8
Rick Carlino <rick.carlino@gmail.com>
2021-11-18 18:50:59 -0600

    Photo grid works :100:

--- examples/new_photo_grid.lua
+++ examples/new_photo_grid.lua
@@ -39,25 +39,28 @@ local cam_rotation = fwe("total_rotation_angle")
 local height_px = fwe("center_pixel_location_y") * 2
 local scale = fwe("coord_scale")
 local width_px = fwe("center_pixel_location_x") * 2
-local x_length = garden_size().x
-local y_length = garden_size().y
 local z = fwe("camera_z")
 local margin_mm = cropAmount(width_px, height_px, cam_rotation)
-local sweep_x = (height_px * scale) - margin_mm
-local sweep_y = (width_px * scale) - margin_mm
-local x_spacing = (scale * fwe("center_pixel_location_y") * 2) - margin_mm
-local y_spacing = (scale * fwe("center_pixel_location_x") * 2) - margin_mm
-local x_max = math.ceil(x_length / x_spacing)
-local y_max = math.ceil(y_length / y_spacing)
+if math.abs(cam_rotation) < 45 then
+    x_spacing = (width_px * scale) - margin_mm
+    y_spacing = (height_px * scale) - margin_mm
+else
+    x_spacing = (height_px * scale) - margin_mm
+    y_spacing = (width_px * scale) - margin_mm
+end
+local x_max = math.ceil(garden_size().x / x_spacing)
+local y_max = math.ceil(garden_size().y / y_spacing)
 
 for y_count = 0, y_max do
     for x_count = 0, x_max do
         if read_status("informational_settings", "locked") then
             return
         else
-            local x = math.floor((sweep_x * x_count) + margin_mm)
-            local y = math.floor((sweep_y * y_count) + margin_mm)
-            move_absolute({x = x, y = y, z = z})
+            move_absolute({
+                x = math.floor(x_spacing * x_count),
+                y = math.floor(y_spacing * y_count),
+                z = z
+            })
             take_photo()
         end
     end
